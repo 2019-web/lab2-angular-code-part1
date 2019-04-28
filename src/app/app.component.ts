71776023efdce3d4611cd6da30b36e20ce8dcbab
FeanLau <2237821788@qq.com>
2019-04-29 06:56:25 +0800

    feat : add .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab2-angular-code';
+  title = 'lab2-angular-code-part1';
 }
